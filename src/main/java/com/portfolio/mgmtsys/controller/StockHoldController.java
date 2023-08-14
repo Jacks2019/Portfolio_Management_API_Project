db667ed51cd2689c8c153c57a4631d832644d59b
JackS2019 <964800156@qq.com>
2023-08-14 10:06:56 +0800

    Code Linted

--- src/main/java/com/portfolio/mgmtsys/controller/StockHoldController.java
+++ src/main/java/com/portfolio/mgmtsys/controller/StockHoldController.java
@@ -6,21 +6,17 @@ package com.portfolio.mgmtsys.controller;
  * Description:
  */
 
-import com.portfolio.mgmtsys.domain.Account;
 import com.portfolio.mgmtsys.model.MyStock;
 import com.portfolio.mgmtsys.service.StockHoldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/stock")
