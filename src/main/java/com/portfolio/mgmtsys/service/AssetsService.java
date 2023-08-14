8c4c8c80cd3ad11ecccb745f887774dd70e81f8a
JackS2019 <964800156@qq.com>
2023-08-14 16:26:09 +0800

    getAssetsById Implemented

--- src/main/java/com/portfolio/mgmtsys/service/AssetsService.java
+++ src/main/java/com/portfolio/mgmtsys/service/AssetsService.java
@@ -1,9 +1,10 @@
 package com.portfolio.mgmtsys.service;
 
+import com.portfolio.mgmtsys.domain.Account;
 import com.portfolio.mgmtsys.domain.Assets;
 
 public interface AssetsService {
-    Assets getAssets(Integer id);
-    Assets transferIn(Integer id);
-    Assets transferOut(Integer id);
+    Assets getAssetsById(Integer id);
+    Assets transferIn(Account account);
+    Assets transferOut(Account account);
 }
--- src/main/java/com/portfolio/mgmtsys/service/impl/AssetsServiceImpl.java
+++ src/main/java/com/portfolio/mgmtsys/service/impl/AssetsServiceImpl.java
@@ -1,22 +1,35 @@
 package com.portfolio.mgmtsys.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.portfolio.mgmtsys.domain.Account;
 import com.portfolio.mgmtsys.domain.Assets;
+import com.portfolio.mgmtsys.repository.AssetsRepo;
 import com.portfolio.mgmtsys.service.AssetsService;
 
 public class AssetsServiceImpl implements AssetsService {
 
+    @Autowired
+    AssetsRepo repo;
+
     @Override
-    public Assets getAssets(Integer id) {
-        throw new UnsupportedOperationException("Unimplemented method 'getAssets'");
+    public Assets getAssetsById(Integer id) {
+        Optional<Assets> assetsOptional = repo.findById(id);
+        if(assetsOptional.isPresent()){
+            return assetsOptional.get();
+        }
+        return null;
     }
 
     @Override
-    public Assets transferIn(Integer id) {
+    public Assets transferIn(Account account) {
         throw new UnsupportedOperationException("Unimplemented method 'transferIn'");
     }
 
     @Override
-    public Assets transferOut(Integer id) {
+    public Assets transferOut(Account account) {
         throw new UnsupportedOperationException("Unimplemented method 'transferOut'");
     }
     
