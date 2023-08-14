bb22724661941bb94876e06fb752864c1a069854
JackS2019 <964800156@qq.com>
2023-08-14 10:26:44 +0800

    Find Stock By Ticker Implemented

--- src/main/java/com/portfolio/mgmtsys/service/StockService.java
+++ src/main/java/com/portfolio/mgmtsys/service/StockService.java
@@ -6,4 +6,6 @@ import com.portfolio.mgmtsys.domain.Stock;
 
 public interface StockService {
     List<Stock> getAllStocks();
+    List<Stock> searchStock();
+    Stock getStockByTicker(String ticker);
 }
--- src/main/java/com/portfolio/mgmtsys/service/impl/StockServiceImpl.java
+++ src/main/java/com/portfolio/mgmtsys/service/impl/StockServiceImpl.java
@@ -1,8 +1,10 @@
 package com.portfolio.mgmtsys.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.portfolio.mgmtsys.domain.Stock;
@@ -17,6 +19,22 @@ public class StockServiceImpl implements StockService {
 
     public List<Stock> getAllStocks() {
         return repo.findAll();
+    }
+
+    @Override
+    public List<Stock> searchStock() {
+        throw new UnsupportedOperationException("Unimplemented method 'searchStock'");
+    }
+
+    @Override
+    public Stock getStockByTicker(String ticker) {
+        Stock tickerStock = new Stock();
+        Example<Stock> tickerExample = Example.of(tickerStock);
+        Optional<Stock> stockOptional = repo.findOne(tickerExample);
+        if(stockOptional.isPresent()){
+            return stockOptional.get();
+        }
+        return null;
     }
     
 }
