5979566ee033f8213e397bf45ab2b1003d1a821e
JackS2019 <964800156@qq.com>
2023-08-14 16:50:47 +0800

    Transfer Recorded

--- src/main/java/com/portfolio/mgmtsys/service/impl/AssetsServiceImpl.java
+++ src/main/java/com/portfolio/mgmtsys/service/impl/AssetsServiceImpl.java
@@ -1,18 +1,25 @@
 package com.portfolio.mgmtsys.service.impl;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.portfolio.mgmtsys.domain.Assets;
+import com.portfolio.mgmtsys.domain.Transfer;
+import com.portfolio.mgmtsys.enumeration.TransferType;
 import com.portfolio.mgmtsys.repository.AssetsRepo;
+import com.portfolio.mgmtsys.repository.TransferRepo;
 import com.portfolio.mgmtsys.service.AssetsService;
 
 public class AssetsServiceImpl implements AssetsService {
 
     @Autowired
     AssetsRepo repo;
+
+    @Autowired
+    TransferRepo transferRepo;
 
     @Override
     public Assets getAssetsById(Integer id) {
@@ -34,6 +41,14 @@ public class AssetsServiceImpl implements AssetsService {
         Assets assets = assetsOptional.get();
         assets.setBalance(assets.getBalance() + amount);
         repo.save(assets);
+
+        Transfer transfer = new Transfer();
+        transfer.setAccountId(id);
+        transfer.setAmount(amount);
+        transfer.setType(TransferType.IN);
+        transfer.setTime(new Date());
+        transferRepo.save(transfer);
+
         return assets;
     }
 
@@ -48,6 +63,14 @@ public class AssetsServiceImpl implements AssetsService {
         Assets assets = assetsOptional.get();
         assets.setBalance(assets.getBalance() - amount);
         repo.save(assets);
+
+        Transfer transfer = new Transfer();
+        transfer.setAccountId(id);
+        transfer.setAmount(amount);
+        transfer.setType(TransferType.OUT);
+        transfer.setTime(new Date());
+        transferRepo.save(transfer);
+
         return assets;
     }
     
