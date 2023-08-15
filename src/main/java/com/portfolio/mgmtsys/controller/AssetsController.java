7e636791962d257374fc8a53c65ef83e0a6a7cb4
JackS2019 <964800156@qq.com>
2023-08-15 11:08:50 +0800

    Transfer In Implemented

--- src/main/java/com/portfolio/mgmtsys/controller/AssetsController.java
+++ src/main/java/com/portfolio/mgmtsys/controller/AssetsController.java
@@ -1,10 +1,14 @@
 package com.portfolio.mgmtsys.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,14 @@ public class AssetsController {
             return new ResponseEntity<Object>("Account not found.", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Object>(assets, HttpStatus.FOUND);
+    }
+
+    @PostMapping("/transferin")
+    public ResponseEntity<Object> transferIn(@RequestBody Map<String, Object> request){
+        Assets assets = service.transferIn(request);
+        if(assets==null){
+            return new ResponseEntity<Object>("Account not found.", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Object>(assets, HttpStatus.ACCEPTED);
     }
 }
