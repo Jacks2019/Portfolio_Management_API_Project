5e79e435252f0e747b2f6774ac716f0292ce1121
JackS2019 <964800156@qq.com>
2023-08-15 15:57:51 +0800

    Controller getfundbycode Implemented

--- src/main/java/com/portfolio/mgmtsys/controller/FundController.java
+++ src/main/java/com/portfolio/mgmtsys/controller/FundController.java
@@ -1,8 +1,8 @@
 package com.portfolio.mgmtsys.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +20,17 @@ public class FundController {
     private FundService service;
 
     @GetMapping("/getallfunds")
-    public List<Fund> getAllFunds() {
-        return service.getAllFunds();
+    public ResponseEntity<Object> getAllFunds() {
+        return new ResponseEntity<Object>(service.getAllFunds(), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/getfundbycode")
+    public ResponseEntity<Object> getFundByCode(@RequestParam String code){
+        Fund fund = service.getFundByCode(code);
+        if(fund==null){
+            return new ResponseEntity<Object>("No fund with this code found.", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Object>(fund, HttpStatus.FOUND);
     }
     
 }
