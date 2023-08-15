c7b6cb389000c050a12be405ee32c21ef2b8594b
JackS2019 <964800156@qq.com>
2023-08-15 15:35:04 +0800

    Fund Repository Added

---
+++ src/main/java/com/portfolio/mgmtsys/repository/FundRepo.java
@@ -0,0 +1,9 @@
+package com.portfolio.mgmtsys.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.portfolio.mgmtsys.domain.Fund;
+
+public interface FundRepo extends JpaRepository<Fund, Integer> {
+    
+}
