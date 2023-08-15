ef543bce4489aaa6e79ba9d774cd5b33f35adb4b
JackS2019 <964800156@qq.com>
2023-08-15 15:51:49 +0800

    Fund Controller Added

---
+++ src/main/java/com/portfolio/mgmtsys/controller/FundController.java
@@ -0,0 +1,11 @@
+package com.portfolio.mgmtsys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fund")
+public class FundController {
+    
+    
+}
