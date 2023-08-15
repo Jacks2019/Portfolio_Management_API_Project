52a63414e0a827502facd739e4abb574b719d56f
JackS2019 <964800156@qq.com>
2023-08-15 15:54:04 +0800

    getAllFunds Controller Implemented

--- src/main/java/com/portfolio/mgmtsys/controller/FundController.java
+++ src/main/java/com/portfolio/mgmtsys/controller/FundController.java
@@ -1,11 +1,27 @@
 package com.portfolio.mgmtsys.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.mgmtsys.domain.Fund;
+import com.portfolio.mgmtsys.service.FundService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/fund")
 public class FundController {
     
+    @Autowired
+    private FundService service;
+
+    @GetMapping("/getallfunds")
+    public List<Fund> getAllFunds() {
+        return service.getAllFunds();
+    }
     
 }
